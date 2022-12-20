a25f9c0c81b755eaf10a1162d7ecaf117762be56
Ataullah Behesti <ataullah.behesti@gmail.com>
2022-12-20 21:59:07 +0200

    Charity finder model created

--- charityfinder/charityfinderapp/models.py
+++ charityfinder/charityfinderapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Charityinformation(models.Model):
+    charity_name = models.CharField(max_length=200)
+    street_name = models.CharField(max_length=200)
+    post_code = models.CharField(max_length=20)
+    district_name = models.CharField(max_length=120)
+    contact_number = models.CharField(max_length=15)
+    extra_info = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = "charityinfo"
